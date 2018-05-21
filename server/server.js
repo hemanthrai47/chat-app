8662f940eb4bd8b03617ba5173a5ac1ff5fe425b
Hemanth <akula@Hemanths-MacBook-Air.local>
2018-05-21 23:38:52 +0530

    chat initial setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+var port = process.env.PORT || 3000;
+var publicPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicPath));
+
+app.listen(port,() => {
+    console.log(`app is running on ${port}`)
+})
+
